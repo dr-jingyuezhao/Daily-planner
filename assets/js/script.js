6ebda3035f66655cf1ba8f0a192a5f4c7d4f0c1e
Jasmine <dr.jingyuezhao@outlook.com>
2023-01-19 21:53:28 +0000

    declare currentDay variable and use moment.js to convert to a specific format

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,3 @@
-alert("yes");
+// Declare currentDay variable and use moment.js to convert to "dddd(Day of Week), DD/MM/YYYY"
+var currentDay = moment();
+$("#currentDay").text(currentDay.format("dddd, DD/MM/YYYY"));

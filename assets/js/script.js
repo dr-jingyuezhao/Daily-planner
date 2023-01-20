0df7c40245dc437d33d1d329a9c3d5bc5d7e84b3
Jasmine <dr.jingyuezhao@outlook.com>
2023-01-20 20:53:41 +0000

    update the time format and add function to check the current hour for colorcoding to align with the class defined in css and add event listener to save button for localstorage

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,15 +1,34 @@
 // Declare currentDay variable and use moment.js to convert to "dddd(Day of Week), DD/MM/YYYY"
-var currentDay = moment();
-$("#currentDay").text(currentDay.format("dddd, DD/MM/YYYY"));
+var todayDate = moment();
+$("#currentDay").text(todayDate.format("dddd, MMMM Do YYYY, h A"));
 
 // Add code to create timeblocks
 // Present timeblocks for standard business hours when the user scrolls down.
 
 // Color-code each timeblock based on past, present, and future when the timeblock is viewed.
 // use moment to check the current time and create a function to compare the viewed timeblock is past,present, and future
-
+function timeChecker() {
+    var nowHour = moment().hour();
+    $(".time-block").each(function () {
+        var blockTime = parseInt($(this).attr("id").split("hour")[1]);
+        if (blockTime < nowHour) {
+            $(this).addClass("past");
+        } else if (blockTime === nowHour) {
+            $(this).addClass("present");
+        } else {
+            $(this).addClass("future");
+        }
+    })
+}
 // Allow a user to enter an event when they click a timeblock.
 
 // Save the event in local storage when the save button is clicked in that timeblock.
-
+$(".saveBtn").on("click", function () {
+    var time = $(this).siblings(".hour").text();
+    var event = $(this).siblings(".description").val();
+    localStorage.setItem(time, event);
+    // console.log("stored");
+});
 // Persist events between refreshes of a page.
+
+timeChecker();

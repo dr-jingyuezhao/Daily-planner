4a8c86f5220e420b3001d92141eed3a1db9bb10d
Jasmine <dr.jingyuezhao@outlook.com>
2023-01-24 15:56:01 +0000

    remove index-1 and script-1

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,24 @@
-// Declare currentDay variable and use moment.js to convert to "dddd(Day of Week), DD/MM/YYYY"
+// Declare currentDay variable and use moment.js to convert to "dddd(Day of Week), MMMM Do YYYY"
 var todayDate = moment();
-$("#currentDay").text(todayDate.format("dddd, MMMM Do YYYY, h:mm:ss A"));
+$("#currentDay").text(todayDate.format("dddd, MMMM Do YYYY"));
 
 // Add code to create timeblocks
 // Present timeblocks for standard business hours when the user scrolls down.
+var containerEl = $(".container");
+var timeblocks = ["9AM", "10AM", "11AM", "12PM", "1PM", "2PM", "3PM", "4PM", "5PM"];
+$(containerEl).html(function () {
+    for (var i = 0; i < timeblocks.length; i++) {
+        var timeblockEl = $(`<div id="hour${i + 9}" class="row time-block">
+        <div class="hour col-1">${timeblocks[i]}</div>
+        <textarea class="col-10 description"></textarea>
+        <button class="btn saveBtn col-1"><i class="fas fa-save"></i></button>
+        </div>`);
+        containerEl.append(timeblockEl);
+    }
+});
 
 // Color-code each timeblock based on past, present, and future when the timeblock is viewed.
-// use moment to check the current time and create a function to compare the viewed timeblock is past,present, and future
+// use moment to check the current time and create a function to compare the viewed timeblock is past, present, and future
 function timeChecker() {
     var nowHour = moment().hour();
     $(".time-block").each(function () {
@@ -20,8 +32,8 @@ function timeChecker() {
         }
     })
 }
-// Allow a user to enter an event when they click a timeblock.
 
+// Allow a user to enter an event when they click a timeblock.
 // Save the event in local storage when the save button is clicked in that timeblock.
 $(".saveBtn").on("click", function () {
     var time = $(this).siblings(".hour").text();
@@ -31,7 +43,7 @@ $(".saveBtn").on("click", function () {
     // add code to display the message: Appointment is added to localStorage
     var header = $(".jumbotron");
     var descriptionEl = $("<span>");
-    descriptionEl.text(time + " appointment is added to localStorage ✔️");
+    descriptionEl.text(time + " appointment is added ✔️");
     descriptionEl.addClass("description");
     header.append(descriptionEl);
 });
